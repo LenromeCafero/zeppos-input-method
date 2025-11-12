5bf4ae8ca417bd3059d3349d9766ee92900f72e7
XiaomaiTX <me@xiaomaitx.com>
2025-11-12 20:16:08 +0800

    init: keyboard-widget

---
+++ src/data-widget/page/index.js
@@ -0,0 +1,22 @@
+import { InputMethod } from '../utils/inputMethod/inputMethod'
+
+Page({
+  build() {
+    console.log("Data Widget Input Method Page build")
+    const inputMethod = new InputMethod({
+      keyboard_list: [0],
+      inputbox_type: [0],
+      max_byte: 50,
+      text: "",
+      title: "输入法"
+    })
+    // inputMethod.start()
+  },
+  onInit() {
+  },
+
+  onDestroy() {
+    // 取消注册手势监听
+    // inputMethod.delete()
+  },
+})
